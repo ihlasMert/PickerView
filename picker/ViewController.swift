af0957317ee745619cfbbbb2881e9cc6cff9597b
İhlas Mert <ihlas1mert@gmail.com>
2021-11-29 16:58:18 +0300

    pick

--- picker/ViewController.swift
+++ picker/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ageTxt: UITextField!
+    let datePicker = UIDatePicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createDatePicker()
     }
-
-
+    func createDatePicker(){
+        
+        ageTxt.textAlignment = .center
+        //toolBar
+        let toolbar = UIToolbar ()
+        toolbar.sizeToFit()
+        //barButton
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        
+        
+        //assign toolbar
+        ageTxt.inputAccessoryView = toolbar
+        //assign date picker to the text field
+        ageTxt.inputView = datePicker
+        //date picker more
+        datePicker.datePickerMode = .date
+    }
+    @objc func donePressed(){
+        
+        //formatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        ageTxt.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+        
+        
+    }
 }
 
